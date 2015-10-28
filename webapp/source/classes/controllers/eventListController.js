7bab3d2460d57241ee1a97ecc7c30d67b0824fe9
gummibjorn <marioegli95@gmail.com>
2015-10-28 15:19:07 +0100

    Change eventListController to use event.js

--- webapp/source/classes/controllers/eventListController.js
+++ webapp/source/classes/controllers/eventListController.js
@@ -1,11 +1,26 @@
-define([], function(){
-  return function($scope){
-    $scope.events = [
-      { name: 'Lunch', place: 'Rapperswil', date: new Date('2015-10-10T10:00:00.000Z') },
-      { name: 'Dinner', place: 'Zürich', date: new Date('2015-04-05T16:00:00.000Z') },
-      { name: 'Dinner', place: 'Rapperswil', date: new Date('2015-12-08T17:00:00.000Z') }
-    ];
-  }
+define(['app/models/event'], function (Event) {
+    return function ($scope) {
+        var event1 = new Event()
+        event1.name='Lunch'
+        event1.location.city='Rapperswil'
+        event1.begin= new Date('2015-10-10T10:00:00.000Z')
+
+        var event2 = new Event()
+        event2.name='Dinner'
+        event2.location.city='Zürich'
+        event2.begin= new Date('2015-04-05T16:00:00.000Z')
+
+        var event3 = new Event()
+        event3.name='Dinner'
+        event3.location.city='Rapperswil'
+        event3.begin= new Date('2015-04-05T16:00:00.000Z')
+
+        $scope.events = [
+            event1,
+            event2,
+            event3
+        ];
+    }
 })
 
 
