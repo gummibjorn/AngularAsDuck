632b84559993e78b0b4892eb532fe7752d9ab133
gummibjorn <marioegli95@gmail.com>
2015-10-29 10:50:39 +0100

    Add static StorageService

--- webapp/source/classes/controllers/eventListController.js
+++ webapp/source/classes/controllers/eventListController.js
@@ -1,25 +1,6 @@
-define(['app/models/event'], function (Event) {
-    return function ($scope) {
-        var event1 = new Event()
-        event1.name='Lunch'
-        event1.location.city='Rapperswil'
-        event1.begin= new Date('2015-10-10T10:00:00.000Z')
-
-        var event2 = new Event()
-        event2.name='Dinner'
-        event2.location.city='Zürich'
-        event2.begin= new Date('2015-04-05T16:00:00.000Z')
-
-        var event3 = new Event()
-        event3.name='Dinner'
-        event3.location.city='Rapperswil'
-        event3.begin= new Date('2015-04-05T16:00:00.000Z')
-
-        $scope.events = [
-            event1,
-            event2,
-            event3
-        ];
+define([], function () {
+    return function ($scope, StorageService) {
+        $scope.events = StorageService.events;
     }
 })
 
--- webapp/source/classes/modules/lafete.js
+++ webapp/source/classes/modules/lafete.js
@@ -1,10 +1,16 @@
 // declare dependency to angular (similar to import in java)
-define(['frameworks/angular', 'app/controllers/eventListController'], function (Angular, EventListController) {
+define(['frameworks/angular', 'app/controllers/eventListController', 'app/services/storageService'], function (Angular, EventListController, StorageService) {
 
-	// Create new empty app/module named 'lafete'
-	var Lafete = Angular.module('lafete', []);
-  Lafete.controller('EventListController', ['$scope', EventListController]);
+    // Create new empty app/module named 'lafete'
+    var Lafete = Angular.module('lafete', []);
 
-	// export module to use it in other classes
-	return Lafete;
+    //services
+    Lafete.service('StorageService', StorageService);
+
+    //controllers
+    EventListController.$inject = ['$scope', 'StorageService'];
+    Lafete.controller('EventListController', EventListController);
+
+    // export module to use it in other classes
+    return Lafete;
 });
